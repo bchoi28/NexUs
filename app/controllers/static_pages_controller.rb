32946779dbf25142782fd28d049365fc0b24e1d3
Brandon Choi <bchoi28@gmail.com>
2023-06-06 16:36:46 -0400

    gitignored backend /public

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,12 @@
+# class StaticPagesController < ActionController::Base
+#   def frontend_index
+#     render file: Rails.root.join('frontend', 'public', 'index.html')
+#     # render file: 'public/index.html'
+#   end
+# end
+
 class StaticPagesController < ActionController::Base
   def frontend_index
-    render file: Rails.root.join('frontend', 'public', 'index.html')
-    # render file: 'public/index.html'
+    render file: Rails.root.join('public', 'index.html')
   end
 end
