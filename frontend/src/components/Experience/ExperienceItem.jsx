3e41d1d1ad604c708474df78c887a270c951664f
Brandon Choi <bchoi28@gmail.com>
2023-07-10 18:18:21 -0400

    Revise ExperienceItem.jsx

--- frontend/src/components/Experience/ExperienceItem.jsx
+++ frontend/src/components/Experience/ExperienceItem.jsx
@@ -1,9 +1,25 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useState, useEffect } from 'react';
 import './ExperienceItem.css';
 import React from 'react';
-
+import { getSessionUser } from '../../store/session';
+import { getUser } from '../../store/user';
+import { openModal } from '../../store/modal';
 
 
 const ExperienceItem = React.memo(() => {
+
+    const dispatch = useDispatch();
+
+    const currentUser = useSelector(getSessionUser);
+    const profileUser = useSelector(getUser);
+
+    const handleEditExperience = (e) => {
+        e.preventDefault();
+        document.body.style.overflow = 'hidden'
+
+        dispatch(openModal('UpdateExperiencesModal', { about: profileUser.about }))
+    }
 
     const experienceEditIcon = (currentUser.id === parseInt(id)) ?
         <i onClick={handleEditExperience} class="edit-experience-button fa-solid fa-pencil"></i> : null;
