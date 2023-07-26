f9720d173a399811e27b8f1e49b0ae314d2d87ca
Brandon Choi <bchoi28@gmail.com>
2023-07-26 17:09:08 -0400

    Add default profile pictures

--- frontend/src/components/Modal/LikeCountModal/LikeCountModal.jsx
+++ frontend/src/components/Modal/LikeCountModal/LikeCountModal.jsx
@@ -1,8 +1,7 @@
 import Modal from 'react-modal';
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
 import { closeModal } from '../../../store/modal';
-import { fetchLikersData, fetchUser } from '../../../store/user';
 import './LikeCountModal.css';
 import { useHistory } from 'react-router-dom';
 
@@ -23,9 +22,10 @@ const LikeCountModal = ({ likers, likeCount }) => {
     }
 
     const likersList = likers.map(liker => {
+        const likerPhoto = liker.photoUrl ? liker.photoUrl : '/assets/images/seeds/default-profile-image-circle.png';
         return (
             <div className='liker-item-container' key={liker.id} onClick={() => handleLinkProfile(liker.id)}>
-                <img className='liker-item-photo' src={liker.photoUrl} alt='liker' />
+                <img className='liker-item-photo' src={likerPhoto} alt='liker' />
                 <div className='liker-item-info'>
                     <div className='liker-item-names'>{liker.fName} {liker.lName}</div>
                     <div className='liker-item-headline'>{liker.headline}</div>
